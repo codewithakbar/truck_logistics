a625d26a54c5a273c2f90ceef9d327809e1fc903
Satipov Akbar <satipovakbar@gmail.com>
2025-01-24 15:11:29 +0500

    add is digit for price

--- src/apps/home/serializers.py
+++ src/apps/home/serializers.py
@@ -6,3 +6,11 @@ class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if "price" in representation:
+            representation["price"] = f"{representation['price']:,}".replace(",", " ")
+
+        return representation
